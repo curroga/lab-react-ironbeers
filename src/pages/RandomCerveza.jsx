24e2a171f6a4249124e9dd232415a25d6bcea401
Curro Garcia <curro-91@hotmail.com>
2022-11-04 21:53:18 +0100

    It5, done

--- src/pages/RandomCerveza.jsx
+++ src/pages/RandomCerveza.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import axios from 'axios'
+
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 
 function RandomCerveza() {
+
+  const [details, setDetails] = useState(null)
+  const [ isFetching, setIsFetching] = useState (true)
+
+  useEffect(() => {
+    getData()    
+  }, [])
+
+  const getData = async () => {
+
+    try {
+      const cerveza = await axios.get("https://ih-beers-api2.herokuapp.com/beers/random")
+      console.log(cerveza.data)
+      setDetails(cerveza.data)
+      setIsFetching(false)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  if(isFetching === true) {
+    return <h3>...buscando</h3>
+  }
   return (
     <div>
        <Link to="/">
@@ -11,6 +38,18 @@ function RandomCerveza() {
       </nav>
        </Link>
       <h4>Nueva Cerveza</h4>
+      <div>
+        <img src={details.image_url} alt={details.name} width='100px' />
+        <div>
+        <h3>{details.name}</h3>
+        <p>{details.tagline}</p>
+        <p>{details.attenuation_level}</p>
+        <p>{details.first_brewed}</p>
+        <p>{details.description}</p>
+        <p>{details.contributed_by}</p>
+        </div>
+      </div>
+
     </div>
   )
 }
